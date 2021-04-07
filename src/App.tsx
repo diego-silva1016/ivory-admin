26830f46d07ff6382cba2cfb1cfe79372bb206d4
Matheus Oliveira <matheus.oliveira@ivoryit.com.br>
2021-04-07 09:51:07 -0300

    Organização de pastas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
-import UserList from './users/UserList';
-import UserCreate from './users/UserCreate';
-import UserEdit from './users/UserEdit';
+import UserList from './pages/users/UserList';
+import UserCreate from './pages/users/UserCreate';
+import UserEdit from './pages/users/UserEdit';
 
 import './styles/global.scss';
-import Login from './login';
+import Login from './pages/login';
 
 function App() {
   const dataProvider = jsonServerProvider('https://localhost:3000/api');
